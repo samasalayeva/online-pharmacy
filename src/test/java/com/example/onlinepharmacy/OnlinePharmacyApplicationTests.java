9b7c5dc7f9b1aa1abf4a02b6b6727bec945825d2
Sama Salayeva <samahs@code.edu.az>
2024-03-14 09:32:01 +0400

    entity added

---
+++ src/test/java/com/example/onlinepharmacy/OnlinePharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinepharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinePharmacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
